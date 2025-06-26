af3d9dc9854283a6e52837002c730a44480d0319
Sudesh Gore <sudeshgore1112@gmail.com>
2025-06-26 20:30:27 +0530

    Max and min values of datatypes

---
+++ CPP/If-Else-Statement/DataTypeLimits.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+
+int main(){
+    cout << "The maximum value for an int is: " << INT_MAX << "\n";
+    cout << "The minimum value for an int is: " << INT_MIN << "\n";
+    cout << "The maximum value for an unsigned int is: " << UINT_MAX << "\n";
+    cout << "The maximum value for an long long is: " << LONG_LONG_MAX << "\n";
+    cout << "The minimum value for an long long is: " << LONG_LONG_MIN << "\n";
+    cout << "The maximum value for an unsigned long long is: " << ULLONG_MAX << "\n";
+    cout << "The maximum value for an char is: " << CHAR_MAX << "\n";
+    cout << "The maximum value for an char is: " << CHAR_MIN << "\n";
+    cout << "The maximum limit of unsigned char is : " << UCHAR_MAX << "\n";
+    cout << "The minimum limit of short is: " << SHRT_MIN << "\n";
+    cout << "The maximum limit of short is: " << SHRT_MAX << "\n";
+    cout << "The maximum limit of unsigned short is:" << USHRT_MAX << "\n";
+}
