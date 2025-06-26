71b976bccd3a370ae3d111535b0ac993b4a4071e
Sudesh Gore <sudeshgore1112@gmail.com>
2025-06-26 21:08:07 +0530

    Printing alphabets

---
+++ CPP/For-loop/PrintAlphabets.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+    //# Print alphabets (lower case letters).
+
+    // char ch = 'a';
+    // for(int i = 0; i <= 25; i++){
+    //     cout << ch  << " ";
+    //     ch++;
+    // }
+
+    //# Print alphabets (upper case letters).
+    char ch = 'A';
+    for (int i = 0; i <= 25; i++)
+    {
+        cout << ch << " ";
+        ch++;
+    }
+
+}
