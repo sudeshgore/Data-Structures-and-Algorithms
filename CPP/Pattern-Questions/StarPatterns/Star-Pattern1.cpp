35ae18de043b5ef68327fe2a95c9b079cc1d73a9
Sudesh Gore <sudeshgore1112@gmail.com>
2025-06-27 22:16:19 +0530

    First star pattern

--- CPP/Pattern-Questions/StarPatterns/Star-Pattern1.cpp
+++ CPP/Pattern-Questions/StarPatterns/Star-Pattern1.cpp
@@ -1,6 +1,26 @@
 #include<iostream>
 using namespace std;
 
+
+/*
+    Patter-1
+        *****
+        *****
+        *****
+        *****
+        *****
+
+*/
 int main(){
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            cout << "*" << " ";
+        }
+        cout << "\n";
+    }
     
 }
