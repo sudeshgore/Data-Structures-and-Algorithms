9ba6ffcdf5046941b7d090a55ea269fb5b656828
Sudesh Gore <sudeshgore1112@gmail.com>
2025-07-08 00:12:46 +0530

    Completed pattern

--- CPP/Pattern-Questions/AlphabeticalPatterns/Alphabetical-Pattern1.cpp
+++ CPP/Pattern-Questions/AlphabeticalPatterns/Alphabetical-Pattern1.cpp
@@ -1,6 +1,22 @@
 #include<iostream>
 using namespace std;
-
+/*
+    Pattern - 1
+        AAAAA
+        BBBBB
+        CCCCC
+        DDDDD
+        EEEEE
+*/
 int main(){
-    
+    char ch = 'A';
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            cout << ch;
+        }
+        ch += 1;
+        cout<<"\n";
+    }
 }
