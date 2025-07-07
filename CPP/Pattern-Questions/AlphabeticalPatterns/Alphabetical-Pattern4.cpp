9ff4c7ff9f0d2a2cd79418c88520c42968a64d48
Sudesh Gore <sudeshgore1112@gmail.com>
2025-07-08 00:36:23 +0530

    Complete pattern

---
+++ CPP/Pattern-Questions/AlphabeticalPatterns/Alphabetical-Pattern4.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    char ch;
+    for (int i = 0; i < 5; i++)
+    {
+        ch = 'E';
+        for (int j = 0; j < 5; j++)
+        {
+            cout << ch;
+            ch--;
+        }
+        cout << "\n";
+    } 
+}
