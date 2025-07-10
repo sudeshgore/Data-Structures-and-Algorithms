359684bb0d129a403ed0a942ed13558c0dca92c9
Sudesh Gore <sudeshgore1112@gmail.com>
2025-07-10 20:48:12 +0530

    Solved

--- CPP/Most asked questions/Leap-year.cpp
+++ CPP/Most asked questions/Leap-year.cpp
@@ -1,17 +1,22 @@
 #include<iostream>
 using namespace std;
 
+bool leapYear(int year){
+    if(year%4==0){
+        if(year % 100 == 0){
+            return year % 400 == 0;
+        }
+        return true;
+    }
+    return false;
+}
 int main(){
     int year;
     cin >> year;
 
-    if(year%4==0){
-        if(year%100 == 0){
-            cout << "Leap year!\n";
-        }
-        else if(year%400==0){
-            cout <<"Not a leap year!\n ";
-        }
+    bool checkLeapYear = leapYear(year);
+    if(checkLeapYear){
+        cout << "Leap year!\n";
     }
     else {
         cout << "Not a leap year1 \n";
