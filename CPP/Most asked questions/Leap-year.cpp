a5ccae872e46d454dc695b17cd5b64a6611d9486
Sudesh Gore <sudeshgore1112@gmail.com>
2025-07-10 20:42:07 +0530

    Solved

--- CPP/Most asked questions/Leap-year.cpp
+++ CPP/Most asked questions/Leap-year.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main(){
-    
+    int year;
+    cin >> year;
+
+    if(year%4==0){
+        if(year%100 == 0){
+            cout << "Leap year!\n";
+        }
+        else if(year%400==0){
+            cout <<"Not a leap year!\n ";
+        }
+    }
+    else {
+        cout << "Not a leap year1 \n";
+    }
 }
