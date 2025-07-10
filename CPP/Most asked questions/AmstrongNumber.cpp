3c9a75aea88616b2be6968b3885b99b181fba508
Sudesh Gore <sudeshgore1112@gmail.com>
2025-07-10 21:29:20 +0530

    Solved exercise

---
+++ CPP/Most asked questions/AmstrongNumber.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >> n;
+    int cnt = 0, temp = n, temp2= n,ans = 0;
+    while(temp > 0){
+        temp /= 10;
+        ++cnt;
+    }
+
+    while(temp2 > 0){
+        int sum = 1;
+        for (int i = 0; i < cnt; i++)
+        {
+            sum *= (temp2%10); 
+        }
+        ans += sum;
+        temp2 /= 10;
+    }
+    if(ans== n){
+        cout<<"Amstrong number!\n";
+    }
+    else {
+        cout << "Not a amstrong number!\n";
+    }
+}
