e6fffb778ed1f91c0ba7f06c19e1edf3bc1d4b06
Sudesh Gore <sudeshgore1112@gmail.com>
2025-07-12 23:28:45 +0530

    Complete exercise

---
+++ CPP/Most asked questions/AreaOfCircle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+#define PI 3.14
+
+int main(){
+    int radius;
+    cin >> radius;
+
+    double area = (double)(PI*radius*radius);
+    cout <<"Area of circle: " << area << "\n";
+}
