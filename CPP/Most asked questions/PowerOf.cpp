d865e18f57d7890f0d315c3d16f76d493cbb5daa
Sudesh Gore <sudeshgore1112@gmail.com>
2025-07-13 23:32:28 +0530

    Complete exercise

---
+++ CPP/Most asked questions/PowerOf.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int value,power;
+    cin >> power;
+    int ans = 1;
+    for (int i = 0; i < power; i++)
+    {
+        ans *= value;
+    }
+    
+}
