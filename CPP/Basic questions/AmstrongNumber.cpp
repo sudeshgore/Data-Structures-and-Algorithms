cc87ab2409a5050f5525ed5312e523ad9759aea5
Sudesh Gore <sudeshgore1112@gmail.com>
2025-07-19 22:50:46 +0530

    Complete exercise

--- CPP/Basic questions/AmstrongNumber.cpp
+++ CPP/Basic questions/AmstrongNumber.cpp
@@ -1,26 +1,39 @@
 #include<iostream>
 using namespace std;
 
+int countDigits(int n){
+    int digits = 0;
+    while(n != 0){
+        ++digits;
+        n /=0;
+    }
+    return digits;
+}
+
+// We can use in-build function 'pow(number, power)' for optimization.
+int findSum(int n, int digits){
+    int ans = 0;
+    while(n > 0){
+        int sum = 1;
+        for (int i = 0; i < digits; i++)
+        {
+            sum *= (n%10); 
+        }
+        ans += sum;
+        n /= 10;
+    }
+    return ans;
+}
+
 int main(){
 
     int n;
     cin >> n;
-    int cnt = 0, temp = n, temp2= n,ans = 0;
-    while(temp > 0){
-        temp /= 10;
-        ++cnt;
-    }
-
-    while(temp2 > 0){
-        int sum = 1;
-        for (int i = 0; i < cnt; i++)
-        {
-            sum *= (temp2%10); 
-        }
-        ans += sum;
-        temp2 /= 10;
-    }
-    if(ans== n){
+    
+    int totalDigits = countDigits(n);
+    int sum = findSum(n, totalDigits);
+    
+    if(sum== n){
         cout<<"Amstrong number!\n";
     }
     else {
