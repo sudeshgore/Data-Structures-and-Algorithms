984559caaf9128a17c289a8b4a5137304695efde
Sudesh Gore <sudeshgore1112@gmail.com>
2025-07-22 23:18:19 +0530

    Arrays

---
+++ Arrays/MinMaxInArray.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int minElement(int nums[], int n){
+    int minElem = nums[0];
+    for (int i = 1; i < n; i++)
+    {
+        if(nums[i] < minElem){
+            minElem = nums[i];
+        }
+    }
+    return minElem;
+}
+
+int maxElement(int nums[], int n){
+    int maxElem = nums[0];
+    for (int i = 1; i < n; i++)
+    {
+        if(nums[i] > maxElem){
+            maxElem = nums[i];
+        }
+    }
+    return maxElem;
+}
+
+int main(){
+    int n; // size of array
+    cin >> n;
+    int nums[n];
+    // Taking input elements in array.
+    for (int i = 0; i < n; i++)
+    {
+        cin >> nums[i];
+    }
+
+    // int minimum = minElement(nums, n);
+    // int maximum = maxElement(nums, n);
+    // cout << "Minimum element is " << minimum << " And " << "maximum element: " << maximum;
+
+    // 2nd method : Using in-build function of C++ STL
+    int maxi = *max_element(nums, nums+n); 
+    int mini = *min_element(nums, nums+n);
+    cout << "Minimum element is "<<mini<<" and Maximum element is "<<maxi << "\n";
+
+}
