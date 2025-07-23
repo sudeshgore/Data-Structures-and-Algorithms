41519783d23d30e31b16a21a19ae4a52d8696e07
Sudesh Gore <sudeshgore1112@gmail.com>
2025-07-23 23:41:24 +0530

    Complete exercise

---
+++ Arrays/FindSumOfElem.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n; // size of array
+    cin >> n;
+    int nums[n];
+    // Taking input elements in array.
+    for (int i = 0; i < n; i++)
+    {
+        cin >> nums[i];
+    }
+
+}
