83201d83556cab3b3db4c62e3fd53b547559e1a4
Sudesh Gore <sudeshgore1112@gmail.com>
2025-08-08 23:08:46 +0530

    Complete exercise

---
+++ Arrays/AreaOfSquare.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int side;
+    cin >> side;
+
+    int areaOfSquare = side*side;
+
+    cout << "Area of square: "<<areaOfSquare;
+}
