bf03ee1588dfb8600bc34741e43a43aeb9c2f0b3
Sudesh Gore <sudeshgore1112@gmail.com>
2026-04-05 17:51:25 +0530

    Iterators

---
+++ STL/Iterators.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector<int> v = {2,3,4,5,6};
+    for(int i = 0; i < v.size(); i++){
+        cout << v[i] << " ";
+    }
+    cout << endl;
+
+    vector<int> ::iterator it = v.begin();
+    for(it = v.begin(); it != v.end(); it++){
+        cout << (*it) << endl;
+    }
+}
