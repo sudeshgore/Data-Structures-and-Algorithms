ed4d0737f32e6805ba92e23152c069524d86722f
Sudesh Gore <sudeshgore1112@gmail.com>
2026-04-05 18:01:17 +0530

    Iterator

--- STL/Iterators.cpp
+++ STL/Iterators.cpp
@@ -8,8 +8,18 @@ int main(){
     }
     cout << endl;
 
-    vector<int> ::iterator it = v.begin();
-    for(it = v.begin(); it != v.end(); it++){
-        cout << (*it) << endl;
+    // vector<int> ::iterator it = v.begin();
+    // for(it = v.begin(); it != v.end(); it++){
+    //     cout << (*it) << endl;
+    // }
+
+    // Iterator of pair of vector
+    vector<pair<int, int> > vp = {{1,2}, {2,3}, {4,5}};
+    vector<pair<int, int>> :: iterator it;
+    for(it = vp.begin(); it != vp.end(); ++it){
+        // cout <<(*it).first << " " << (*it).second;
+        // Or
+        cout << (it->first) << " " << (it->second);
     }
+    
 }
